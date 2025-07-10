78733f1cc9ec8d49f1bc1913344eff40c00c240d
psrikar045 <psrikar@stratapps.com>
2025-07-11 01:09:40 +0530

    version change, angular change, assets added, toolbar fix

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -34,7 +34,7 @@ export class ToolbarComponent implements OnInit, OnDestroy {
   private themeService = inject(ThemeService);
   private iconRegistry = inject(MatIconRegistry);
   private sanitizer = inject(DomSanitizer);
-  @Inject(PLATFORM_ID) private platformId: Object;
+  @Inject(PLATFORM_ID) private platformId!: Object;
 
   // Theme properties
   isDarkMode: boolean = false;
@@ -58,11 +58,11 @@ export class ToolbarComponent implements OnInit, OnDestroy {
   // private resizeSubscription!: Subscription; // Replaced by @HostListener as per previous iteration
 
   // Icon paths
-  private logoPath = 'assets/images/logo.svg';
-  private sunIconPath = 'assets/icons/sun.svg';
-  private cloudIconPath = 'assets/icons/cloud.svg';
-  private arrowForwardIconPath = 'assets/icons/arrow_forward.svg';
-  private menuIconPath = 'assets/icons/menu.svg';
+  private logoPath = 'images/logo.svg';
+  private sunIconPath = 'icons/sun.svg';
+  private cloudIconPath = 'icons/cloud.svg';
+  private arrowForwardIconPath = 'icons/arrow_forward.svg';
+  private menuIconPath = 'icons/menu.svg';
 
   constructor() {
     this.registerIcons();
@@ -71,7 +71,7 @@ export class ToolbarComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.themeSubscription = this.themeService.isDarkMode$.pipe(
       // Ensure isDarkMode$ in ThemeService is BehaviorSubject or similar to get current value
-      startWith(this.themeService.isDarkMode$.getValue()),
+      // startWith(this.themeService.isDarkMode$.getValue()),
       takeUntil(this.destroy$)
     ).subscribe(isDark => {
       this.isDarkMode = isDark;
