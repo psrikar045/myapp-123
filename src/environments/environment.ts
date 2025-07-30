882db09fd2ec0414808958a3977dab7cd4428c91
psrikar045 <psrikar@stratapps.com>
2025-07-30 16:35:44 +0530

    environment code changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   baseApiUrl: 'http://202.65.155.125:8080/myapp',// deployed
   // baseApiUrl: 'http://localhost:8080/myapp',// local
   logLevel: 'error', // Typically 'error' or 'warn' for production
